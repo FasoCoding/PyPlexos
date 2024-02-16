dd55690ff6ecbe21f981cdc9f3bc339e62de1367
FasoCoding <bastidas.felipe@google.com>
2024-02-16 17:24:28 -0300

    Refactoring: Facade pattern

--- pyplexos/main.py
+++ pyplexos/main.py
@@ -12,13 +12,16 @@ if __name__ == "__main__":
     print(
         f"Modelo extraido en {end - start:.2f} segundos \nIniciando guardado en parquet..."
     )
-    #reader.to_parquet(path_to_dir=r"data\Parquet")
+
+    parquet_writer = PlexosWriter.parquet_writer(path_to_dir=r"data\Parquet")
+    parquet_writer.write(reader)
     end_parquet = time.time()
     print(
         f"termino! guardado en {end_parquet - end:.2f} segundos \nTiempo total: {end_parquet - start:.2f} segundos"
     )
-    writer = PlexosWriter.duck_writer(path_to_dir=r"data\DuckDB", db_name="test_pcp.ddb")
-    writer.to_duck(reader)
+
+    duck_writer = PlexosWriter.duck_writer(path_to_dir=r"data\DuckDB", db_name="test_pcp.ddb")
+    duck_writer.write(reader)
     end_duck = time.time()
     print(
         f"termino! guardado en {end_duck - end_parquet:.2f} segundos \nTiempo total: {end_duck - start:.2f} segundos"
---
+++ pyplexos/writer/model/parquet_writer.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from dataclasses import dataclass
+from typing import Self
+
+from pyplexos.Protocols import PlexosReaderProtocol
+
+import polars as pl
+
+
+@dataclass
+class ParquetWriter:
+    path_to_dir: Path
+
+    @classmethod
+    def parquet_writer(cls, path_to_dir: str) -> Self:
+        temp_path = Path(path_to_dir)
+        if not temp_path.exists():
+            raise ValueError(f"Path: {path_to_dir} does not exists.")
+        return cls(path_to_dir=temp_path)
+    
+    def write(self, solution: PlexosReaderProtocol) -> None:
+        for table_name, table_data in solution.get_solution_model.items():
+            if table_data is None:
+                continue
+            file = self.path_to_dir / table_name
+            pl.from_dicts(table_data).write_parquet(file.with_suffix(".parquet"))
+
+        for table_name, table_data in solution.get_solution_data.items():
+            if table_data is None:
+                continue
+            file = self.path_to_dir / table_name
+            pl.from_dict(table_data).write_parquet(file.with_suffix(".parquet"))
