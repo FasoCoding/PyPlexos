eb44dc5b06a7ab034243ce359a538b8fc237e6cc
FasoCoding <bastidas.felipe@google.com>
2024-04-05 13:06:34 -0300

    Se comenta la clase. En proceos de definir su futuro.

--- pyplexos/reader/__init__.py
+++ pyplexos/reader/__init__.py
@@ -1,39 +1,41 @@
-from typing import Self
-from dataclasses import dataclass
+#from typing import Self
+#from dataclasses import dataclass
 
 from pyplexos.reader.zip import PlexosZipReader
-from pyplexos.protocols import PlexosReaderProtocol
+#from pyplexos.protocols import PlexosReaderProtocol
+
+#import pyplexos.reader.zip
 
 
-@dataclass
-class PlexosReader:
-    """
-    A class for reading plexos data from a ZIP solution file containing XML
-    and binary files. On creation checks for existing files.
-    """
-
-    solution_reader: PlexosReaderProtocol
-
-    @classmethod
-    def zip_reader(cls, path: str) -> Self:
-        """
-        Initialize a PlexosZipReader object from a zip file.
-
-        Args:
-            zip_file_path (str): The path to the ZIP file.
-        """
-        return cls(solution_reader=PlexosZipReader.read(zip_path=path))
-
-    # TODO. check si un yield se puede usar para bajar computo.
-    #@property
-    #def get_solution_model(self) -> dict[str, list[dict[str, Any]]]:
-    #    return self.solution_reader.get_solution_model
-
-    # TODO. check si un yield se puede usar para bajar computo.
-    #@property
-    #def get_solution_data(self) -> dict[str, dict[str, list[Any]]]:
-    #    return self.solution_reader.get_solution_data
-
-    #@property
-    #def get_initial_datetime(self) -> datetime:
-    #    return self.solution_reader.get_initial_datetime
+#@dataclass
+#class PlexosReader:
+#    """
+#    A class for reading plexos data from a ZIP solution file containing XML
+#    and binary files. On creation checks for existing files.
+#    """
+#
+#    solution_reader: PlexosReaderProtocol
+#
+#    @classmethod
+#    def zip_reader(cls, path: str) -> Self:
+#        """
+#        Initialize a PlexosZipReader object from a zip file.
+#
+#        Args:
+#            zip_file_path (str): The path to the ZIP file.
+#        """
+#        return cls(solution_reader=PlexosZipReader.read(zip_path=path))
+#
+#    # TODO. check si un yield se puede usar para bajar computo.
+#    #@property
+#    #def get_solution_model(self) -> dict[str, list[dict[str, Any]]]:
+#    #    return self.solution_reader.get_solution_model
+#
+#    # TODO. check si un yield se puede usar para bajar computo.
+#    #@property
+#    #def get_solution_data(self) -> dict[str, dict[str, list[Any]]]:
+#    #    return self.solution_reader.get_solution_data
+#
+#    #@property
+#    #def get_initial_datetime(self) -> datetime:
+#    #    return self.solution_reader.get_initial_datetime
