eabcb291454f282b9a1971795aa6ddaa20065649
FasoCoding <bastidas.felipe@google.com>
2024-06-07 14:23:03 -0400

    actualización writer

--- pyplexos/writer/duckdb/__init__.py
+++ pyplexos/writer/duckdb/__init__.py
@@ -2,19 +2,21 @@ from pathlib import Path
 from typing import Any
 
 import duckdb as duck
-import pyarrow as pa
 
+from pyplexos.solution.schema import SolutionProtocol
 
 def write_duckdb(
-    path_to_db: str, table_dict: dict[str, pa.Table], **kwargs: Any
+    path_to_db: str,
+    solution: SolutionProtocol,
+    **kwargs: Any
 ) -> None:
     path = Path(path_to_db)
-    db_name = "prg.duckdb"
+    db_name = "raw.duck"
 
     if "db_name" in kwargs:
-        db_name = kwargs["db_name"]
+        db_name: str = kwargs["db_name"]
 
-    if not path.exists():
+    if not path.exists() and not path.is_dir():
         raise FileNotFoundError(f"Path does not exists: {path_to_db}")
 
     
@@ -22,9 +24,9 @@ def write_duckdb(
     
     conn = duck.connect(db_path.as_posix())
 
-    for name, data in table_dict.items():
-        if data.num_columns == 0:
+    for table_name, table_data in solution.items():
+        if table_data.num_columns == 0:
             continue 
-        conn.from_arrow(data).create(table_name=name)
+        conn.from_arrow(table_data).create(table_name=table_name)
     
     conn.close()
