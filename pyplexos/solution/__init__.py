aff5e91759eb8a4c93c3e608b875e57aa071e9c4
FasoCoding <bastidas.felipe@google.com>
2024-06-07 14:22:40 -0400

    rework plexos solution

---
+++ pyplexos/solution/__init__.py
@@ -0,0 +1,302 @@
+from dataclasses import dataclass, fields
+from enum import Enum
+from pathlib import Path
+from typing import Any, Self
+
+import polars as pl
+import pyarrow as pa
+
+from pyplexos.reader.accdb import create_accdb_engine, get_data
+from pyplexos.reader.zip import extract_zip_data
+
+# from pyplexos.model.query import QueryCollection, QueryProperty
+from pyplexos.solution.schema import QuerySchema
+from pyplexos.solution.schema import SolutionSchema as SolSch
+from pyplexos.writer.duckdb import write_duckdb
+from pyplexos.writer.parquet import write_parquet
+
+
+@dataclass
+class PlexosSolution:
+    t_unit: pa.Table
+    t_band: pa.Table
+    t_category: pa.Table
+    t_class: pa.Table
+    t_class_group: pa.Table
+    t_collection: pa.Table
+    t_config: pa.Table
+    t_key: pa.Table
+    t_membership: pa.Table
+    t_model: pa.Table
+    t_object: pa.Table
+    t_period_0: pa.Table
+    t_period_1: pa.Table
+    t_period_2: pa.Table
+    t_period_3: pa.Table
+    t_period_4: pa.Table
+    t_period_6: pa.Table
+    t_period_7: pa.Table
+    t_phase_1: pa.Table
+    t_phase_2: pa.Table
+    t_phase_3: pa.Table
+    t_phase_4: pa.Table
+    t_sample: pa.Table
+    t_timeslice: pa.Table
+    t_key_index: pa.Table
+    t_property: pa.Table
+    t_attribute_data: pa.Table
+    t_attribute: pa.Table
+    t_sample_weight: pa.Table
+    t_custom_column: pa.Table
+    t_memo_object: pa.Table
+    t_object_meta: pa.Table
+    t_data_0: pa.Table
+
+    def items(self):
+        for field in fields(self):
+            yield field.name, getattr(self, field.name)
+
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+    @classmethod
+    def from_zip(cls, zip_file_path: str) -> Self:
+        path = Path(zip_file_path)
+
+        if not path.exists():
+            raise FileNotFoundError(f"Path does not exists: {zip_file_path}")
+
+        data = extract_zip_data(path=path)
+
+        return cls(
+            t_unit=pa.Table.from_pylist(data[SolSch.t_unit.value]),
+            t_band=pa.Table.from_pylist(data[SolSch.t_band.value]),
+            t_category=pa.Table.from_pylist(data[SolSch.t_category.value]),
+            t_class=pa.Table.from_pylist(data[SolSch.t_class.value]),
+            t_class_group=pa.Table.from_pylist(data[SolSch.t_class_group.value]),
+            t_collection=pa.Table.from_pylist(data[SolSch.t_collection.value]),
+            t_config=pa.Table.from_pylist(data[SolSch.t_config.value]),
+            t_key=pa.Table.from_pylist(data[SolSch.t_key.value]),
+            t_membership=pa.Table.from_pylist(data[SolSch.t_membership.value]),
+            t_model=pa.Table.from_pylist(data[SolSch.t_model.value]),
+            t_object=pa.Table.from_pylist(data[SolSch.t_object.value]),
+            t_period_0=pa.Table.from_pylist(data[SolSch.t_period_0.value]),
+            t_period_1=pa.Table.from_pylist(data[SolSch.t_period_1.value]),
+            t_period_2=pa.Table.from_pylist(data[SolSch.t_period_2.value]),
+            t_period_3=pa.Table.from_pylist(data[SolSch.t_period_3.value]),
+            t_period_4=pa.Table.from_pylist(data[SolSch.t_period_4.value]),
+            t_period_6=pa.Table.from_pylist(data[SolSch.t_period_6.value]),
+            t_period_7=pa.Table.from_pylist(data[SolSch.t_period_7.value]),
+            t_phase_1=pa.Table.from_pylist(data[SolSch.t_phase_1.value]),
+            t_phase_2=pa.Table.from_pylist(data[SolSch.t_phase_2.value]),
+            t_phase_3=pa.Table.from_pylist(data[SolSch.t_phase_3.value]),
+            t_phase_4=pa.Table.from_pylist(data[SolSch.t_phase_4.value]),
+            t_sample=pa.Table.from_pylist(data[SolSch.t_sample.value]),
+            t_timeslice=pa.Table.from_pylist(data[SolSch.t_timeslice.value]),
+            t_key_index=pa.Table.from_pylist(data[SolSch.t_key_index.value]),
+            t_property=pa.Table.from_pylist(data[SolSch.t_property.value]),
+            t_attribute_data=pa.Table.from_pylist(data[SolSch.t_attribute_data.value]),
+            t_attribute=pa.Table.from_pylist(data[SolSch.t_attribute.value]),
+            t_sample_weight=pa.Table.from_pylist(data[SolSch.t_sample_weight.value]),
+            t_custom_column=pa.Table.from_pylist(data[SolSch.t_custom_column.value]),
+            t_memo_object=pa.Table.from_pylist(data[SolSch.t_memo_object.value]),
+            t_object_meta=pa.Table.from_pylist(data[SolSch.t_object_meta.value]),
+            t_data_0=pa.Table.from_pydict(data[SolSch.t_data_0.value]),
+        )
+
+    @classmethod
+    def from_accdb(cls, accdb_file_path: str) -> Self:
+        path = Path(accdb_file_path)
+
+        if not path.exists():
+            raise FileNotFoundError(f"Path does not exists: {accdb_file_path}")
+
+        accdb_engine = create_accdb_engine(path)
+
+        with accdb_engine.connect() as conn:
+            data = {
+                SolSch.t_unit.value: get_data(SolSch.t_unit.value, conn),
+                SolSch.t_band.value: get_data(SolSch.t_band.value, conn),
+                SolSch.t_category.value: get_data(SolSch.t_category.value, conn),
+                SolSch.t_class.value: get_data(SolSch.t_class.value, conn),
+                SolSch.t_class_group.value: get_data(SolSch.t_class_group.value, conn),
+                SolSch.t_collection.value: get_data(SolSch.t_collection.value, conn),
+                SolSch.t_config.value: get_data(SolSch.t_config.value, conn),
+                SolSch.t_key.value: get_data(SolSch.t_key.value, conn),
+                SolSch.t_membership.value: get_data(SolSch.t_membership.value, conn),
+                SolSch.t_model.value: get_data(SolSch.t_model.value, conn),
+                SolSch.t_object.value: get_data(SolSch.t_object.value, conn),
+                SolSch.t_period_0.value: get_data(SolSch.t_period_0.value, conn),
+                SolSch.t_period_1.value: get_data(SolSch.t_period_1.value, conn),
+                SolSch.t_period_2.value: get_data(SolSch.t_period_2.value, conn),
+                SolSch.t_period_3.value: get_data(SolSch.t_period_3.value, conn),
+                SolSch.t_period_4.value: get_data(SolSch.t_period_4.value, conn),
+                SolSch.t_period_6.value: get_data(SolSch.t_period_6.value, conn),
+                SolSch.t_period_7.value: get_data(SolSch.t_period_7.value, conn),
+                SolSch.t_phase_1.value: get_data(SolSch.t_phase_1.value, conn),
+                SolSch.t_phase_2.value: get_data(SolSch.t_phase_2.value, conn),
+                SolSch.t_phase_3.value: get_data(SolSch.t_phase_3.value, conn),
+                SolSch.t_phase_4.value: get_data(SolSch.t_phase_4.value, conn),
+                SolSch.t_sample.value: get_data(SolSch.t_sample.value, conn),
+                SolSch.t_timeslice.value: get_data(SolSch.t_timeslice.value, conn),
+                SolSch.t_key_index.value: get_data(SolSch.t_key_index.value, conn),
+                SolSch.t_property.value: get_data(SolSch.t_property.value, conn),
+                SolSch.t_attribute_data.value: get_data(
+                    SolSch.t_attribute_data.value, conn
+                ),
+                SolSch.t_attribute.value: get_data(SolSch.t_attribute.value, conn),
+                SolSch.t_sample_weight.value: get_data(
+                    SolSch.t_sample_weight.value, conn
+                ),
+                SolSch.t_custom_column.value: get_data(
+                    SolSch.t_custom_column.value, conn
+                ),
+                SolSch.t_memo_object.value: get_data(SolSch.t_memo_object.value, conn),
+                SolSch.t_object_meta.value: get_data(SolSch.t_object_meta.value, conn),
+                SolSch.t_data_0.value: get_data(SolSch.t_data_0.value, conn),
+            }
+
+        return cls(**data)
+
+    def to_parquet(self, path: str, **kwargs: Any) -> None:
+        write_parquet(path_to_folder=path, solution=self, **kwargs)
+
+    def to_duck(self, path: str, **kwargs: Any) -> None:
+        write_duckdb(path_to_db=path, solution=self, **kwargs)
+
+    def query(self, query_enum: Enum) -> pl.DataFrame:
+        t_membership: pl.LazyFrame = pl.from_arrow(self.t_membership).lazy()  # type: ignore
+        t_parent_object: pl.LazyFrame = pl.from_arrow(self.t_object).lazy()  # type: ignore
+        t_child_object: pl.LazyFrame = pl.from_arrow(self.t_object).lazy()  # type: ignore
+        t_property: pl.LazyFrame = pl.from_arrow(self.t_property).lazy()  # type: ignore
+        t_collection: pl.LazyFrame = pl.from_arrow(self.t_collection).lazy()  # type: ignore
+        t_category: pl.LazyFrame = pl.from_arrow(self.t_category).lazy()  # type: ignore
+        t_key: pl.LazyFrame = pl.from_arrow(self.t_key).lazy()  # type: ignore
+        t_data: pl.LazyFrame = pl.from_arrow(self.t_data_0).lazy()  # type: ignore
+        t_period: pl.LazyFrame = pl.from_arrow(self.t_period_0).lazy()  # type: ignore
+        phase = t_key.select("phase_id").collect().item(0, 0)  # type: ignore
+        match phase:
+            case 1:
+                t_phase: pl.LazyFrame = pl.from_arrow(self.t_phase_1).lazy()  # type: ignore
+            case 2:
+                t_phase: pl.LazyFrame = pl.from_arrow(self.t_phase_2).lazy()  # type: ignore
+            case 3:
+                t_phase: pl.LazyFrame = pl.from_arrow(self.t_phase_3).lazy()  # type: ignore
+            case 4:
+                t_phase: pl.LazyFrame = pl.from_arrow(self.t_phase_4).lazy()  # type: ignore
+
+        data = (
+            t_membership.join(
+                t_collection,
+                on=["collection_id", "parent_class_id", "child_class_id"],
+            )
+            .select(pl.exclude(["complement_name", "lang_id"]))
+            .join(
+                t_parent_object,
+                left_on="parent_object_id",
+                right_on="object_id",
+                suffix="_parent",
+            )
+            .select(pl.exclude(["class_id", "category_id", "index", "show", "GUID"]))
+            .join(
+                t_child_object,
+                left_on="child_object_id",
+                right_on="object_id",
+                suffix="_child",
+            )
+            .select(pl.exclude(["class_id", "index", "show", "GUID"]))
+            .join(t_property, on="collection_id", suffix="_property")
+            .select(
+                pl.exclude(
+                    [
+                        "enum_id",
+                        "summary_name",
+                        "summary_unit_id",
+                        "is_multi_band",
+                        "is_period",
+                        "is_summary",
+                        "lang_id",
+                        "unit_id",
+                    ]
+                )
+            )
+            .join(t_key, on=["membership_id", "property_id"])
+            .select(
+                pl.exclude(
+                    [
+                        "model_id",
+                        "period_type_id",
+                        "band_id",
+                        "sample_id",
+                        "timeslice_id",
+                    ]
+                )
+            )
+            .join(t_data, on="key_id")
+            .join(t_phase, on="period_id")
+            .join(
+                t_period.select(["interval_id", "day_id", "period_of_day", "datetime"]),
+                on="interval_id",
+            )
+            .join(
+                t_category.select(["category_id", "name"]),
+                on="category_id",
+                suffix="_category",
+            )
+            .rename(
+                {
+                    "name": "collection_name",
+                    "name_parent": "parent_name",
+                    "name_child": "child_name",
+                    "name_property": "property_name",
+                    "name_category": "category_name",
+                    "day_id": "day",
+                    "period_of_day": "hour",
+                }
+            )
+        )
+        collection_value, _, property_name = query_enum.value
+        return (
+            data.filter(
+                pl.col("collection_id").eq(collection_value),
+                pl.col("property_name").eq(property_name),
+            )
+            .select(
+                [
+                    "collection_name",
+                    "category_name",
+                    "parent_name",
+                    "child_name",
+                    "property_name",
+                    "datetime",
+                    "value",
+                ]
+            )
+            .collect()
+        )
+
+    @property
+    def gen(self) -> pl.DataFrame:
+        return self.query(QuerySchema.GENERATOR.GENERATION)
+
+    @property
+    def cmg(self) -> pl.DataFrame:
+        return self.query(QuerySchema.NODE.PRICE)
+
+    @property
+    def flow(self) -> pl.DataFrame:
+        return self.query(QuerySchema.LINE.FLOW)
+
+    @property
+    def load(self) -> pl.DataFrame:
+        return self.query(QuerySchema.NODE.LOAD)
+
+    @property
+    def reserve(self) -> pl.DataFrame:
+        return self.query(QuerySchema.RESERVE.PROVISION)
+
+    @property
+    def va(self) -> pl.DataFrame:
+        return self.query(QuerySchema.STORAGE.SHADOW_PRICE).with_columns(
+            (pl.col("value") * 1000 / 86400).alias("value")
+        )
---
+++ pyplexos/solution/schema.py
@@ -0,0 +1,126 @@
+from enum import Enum
+from typing import Protocol, Generator
+from dataclasses import dataclass
+
+import pyarrow as pa
+
+class SolutionSchema(Enum):
+    t_unit = "t_unit"
+    t_band = "t_band"
+    t_category = "t_category"
+    t_class = "t_class"
+    t_class_group = "t_class_group"
+    t_collection = "t_collection"
+    t_config = "t_config"
+    t_key = "t_key"
+    t_membership = "t_membership"
+    t_model = "t_model"
+    t_object = "t_object"
+    t_period_0 = "t_period_0"
+    t_period_1 = "t_period_1"
+    t_period_2 = "t_period_2"
+    t_period_3 = "t_period_3"
+    t_period_4 = "t_period_4"
+    t_period_6 = "t_period_6"
+    t_period_7 = "t_period_7"
+    t_phase_1 = "t_phase_1"
+    t_phase_2 = "t_phase_2"
+    t_phase_3 = "t_phase_3"
+    t_phase_4 = "t_phase_4"
+    t_sample = "t_sample"
+    t_timeslice = "t_timeslice"
+    t_key_index = "t_key_index"
+    t_property = "t_property"
+    t_attribute_data = "t_attribute_data"
+    t_attribute = "t_attribute"
+    t_sample_weight = "t_sample_weight"
+    t_custom_column = "t_custom_column"
+    t_memo_object = "t_memo_object"
+    t_object_meta = "t_object_meta"
+    t_data_0 = "t_data_0"
+
+class SolutionProtocol(Protocol):
+    def items(self) -> Generator[tuple[str, pa.Table], None, None]:
+        ...
+
+class GeneratorProperty(Enum):
+    GENERATION = 1, 1, "Generation"
+    UNITS_GENERATING = 1, 6, "Units Generating"
+    DISPATCHABLE_CAPACITY = 1, 9, "Dispatchable Capacity"
+    UNDISPATCHABLE_CAPACITY = 1, 10, "Undispatched Capacity"
+    CAPACITY_CURTAILED = 1, 28, "Capacity Curtailed"
+    AUXILIARY_USE = 1, 74, "Auxiliary Use"
+    PUMP_LOAD = 1, 78, "Pump Load"
+    GENERATION_COST = 1, 104, "Generation Cost"
+    START_STOP_COST = 1, 105, "Start & Shutdown Cost"
+    TOTAL_GENERATION_COST = 1, 112, "Total Generation Cost"
+    SRMC = 1, 123, "SRMC"
+    MAX_CAPACITY = 1, 200, "Max Capacity"
+    UNITS_OUT = 1, 209, "Units Out"
+    AVAILABLE_CAPACITY = 1, 219, "Available Capacity"
+
+class FuelProperty(Enum):
+    OFFTAKE = 33, 344, "Offtake"
+    SHADOW_PRICE = 33, 361, "Shadow Price"
+
+class StorageProperty(Enum):
+    MAX_VOLUME = 80, 478, "Max Volume"
+    MIN_VOLUME = 80, 479, "Min Volume"
+    INITIAL_VOLUME = 80, 480, "Initial Volume"
+    END_VOLUME = 80, 481, "End Volume"
+    NATURAL_INFLOW = 80, 492, "Natural Inflow"
+    SHADOW_PRICE = 80, 505, "Shadow Price"
+    NON_PHYSICAL_INFLOW = 80, 524, "Non-physical Inflow"
+
+class WaterwayProperty(Enum):
+    FLOW = 88, 529, "Flow"
+    MAX_FLOW = 88, 530, "Max Flow"
+    MIN_FLOW = 88, 531, "Min Flow"
+    SHADOW_PRICE = 88, 533, "Shadow Price"
+
+class ReserveProperty(Enum):
+    PROVISION = 115, 586, "Provision"
+    SHORTAGE = 115, 588, "Shortage"
+    CLEARED_OFFER_PRICE = 115, 591, "Cleared Offer Price"
+    PRICE = 115, 594, "Price"
+    AVAILABLE_RESPONSE = 115, 596, "Available Response"
+
+class NodeProperty(Enum):
+    LOAD = 245, 1200, "Load"
+    GENERATION = 245, 1204, "Generation"
+    UNSERVED_ENERGY = 245, 1229, "Unserved Energy"
+    LOSSES = 245, 1231, "Losses"
+    PRICE = 245, 1233, "Price"
+    MARGINAL_LOSS_FACTOR = 245, 1237, "Marginal Loss Factor"
+    PHASE_ANGLE = 245, 1239, "Phase Angle"
+
+class LineProperty(Enum):
+    FLOW = 264, 1274, "Flow"
+    EXPORT_LIMIT = 264, 1277, "Export Limit"
+    IMPORT_LIMIT = 264, 1278, "Import Limit"
+    HOURS_CONGESTED = 264, 1281, "Hours Congested"
+    RENTAL = 264, 1303, "Rental"
+    LOSS = 264, 1307, "Loss"
+    UNITS_OUT = 264, 1328, "Units Out"
+
+class ConstraintProperty(Enum):
+    ACTIVITY = 635, 2642, "Activity"
+    SLACK = 635, 2643, "Slack"
+    HOURS_BINDING = 635, 2645, "Hours Binding"
+    RHS = 635, 2646, "RHS"
+    PRICE = 635, 2647, "Price"
+
+class DecisionVariableProperty(Enum):
+    Value = 642, 2660, "value"
+
+@dataclass
+class QuerySchema:
+    GENERATOR = GeneratorProperty
+    FUEL = FuelProperty
+    STORAGE = StorageProperty
+    WATERWAY = WaterwayProperty
+    RESERVE = ReserveProperty
+    NODE = NodeProperty
+    LINE = LineProperty
+    CONSTRAINT = ConstraintProperty
+    DECISION_VARIABLE = DecisionVariableProperty
